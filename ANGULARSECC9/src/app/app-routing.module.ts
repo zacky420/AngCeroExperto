adaff4a8583054770306ebe73123cfa55c1331c7
zacky420 <albertoserrano7@hotmail.com>
2024-04-11 15:13:25 +0200

    hacemos de cero el proyecto :(

--- ANGULARSECC9/src/app/app-routing.module.ts
+++ ANGULARSECC9/src/app/app-routing.module.ts
@@ -1,29 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './shared/Pages/home-page/home-page.component';
-import { AboutPageComponent } from './shared/Pages/about-page/about-page.component';
-import { ContactUsComponent } from './shared/Pages/contact-us/contact-us.component';
-import { CountriesModule } from './countries/countries.module';
 
-const routes: Routes = [
-// {
-//   path: "",component: HomePageComponent,
-// },
-{
-  path : "about",component:AboutPageComponent
-},
-{
-  path : "contact-us",component:ContactUsComponent
-},
-{
-  path : "countries",
-loadChildren: () => import ("./countries/countries.module").then (m => m.CountriesModule)
-},
-{
-  path : "**",redirectTo:"countries/by-capital"
-}
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ANGULARSECC9/src/app/app.module.ts
+++ ANGULARSECC9/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,10 +10,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SharedModule
+    AppRoutingModule
   ],
-
   providers: [],
   bootstrap: [AppComponent]
 })
